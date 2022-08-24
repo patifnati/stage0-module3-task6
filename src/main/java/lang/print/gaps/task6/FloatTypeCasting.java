445677fc83b2bcf4f92e323e2d5c894f27a99aa4
patifnati <rzezniczekpatrycja@gmail.com>
2022-08-24 21:46:25 +0200

    task done

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task6;
 
-public class FloatTypeCasting {
+import java.math.RoundingMode;
+
+public class          FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+    int roundNumber = (int) Math.round(numberToBeRounded);
+
     }
 }
